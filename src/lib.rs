29cc76640cea39b8149c00bfd6bf97ff2153342d
Stefan Rabmund <sorcerersr@mailbox.org>
2021-05-14 17:54:43 +0200

    fix long lines

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 //! # term-snip
 //!
-//! `term-snip` A small utility crate using https://crates.io/crates/console 
-//! to write to stdout but limited to a given number of lines. 
+//! `term-snip` A small utility crate using https://crates.io/crates/console
+//! to write to stdout but limited to a given number of lines.
 //! The oldest line is removed when writing a new line.
 //!
 //!## Usage
@@ -14,7 +14,7 @@
 //!use std::{thread, time};
 //!
 //!/// A simple example writing 15 lines to stdout but only showing
-//!/// a maximum of five lines. 
+//!/// a maximum of five lines.
 //!fn main() {
 //!    let half_sec = time::Duration::from_millis(500);
 //!    
@@ -22,11 +22,11 @@
 //!    for n in 1..15 {
 //!        term.write_line(&format!("{} - line number {}", n, n)).unwrap();
 //!        
-//!        // just to slow down for demonstration 
+//!        // just to slow down for demonstration
 //!        thread::sleep(half_sec);
 //!    }
 //!}
-//! 
+//!
 //!```
 //!
 //!
@@ -48,16 +48,14 @@ use console::Term;
 /// Representation of a terminal.
 pub struct TermSnip {
     term: Term,
-    limit: usize, 
+    limit: usize,
     fill: bool,
     lines: VecDeque<String>,
-    
 }
 
-
-impl TermSnip{
+impl TermSnip {
     /// Creates a TermSnip wich limits output lines to the given limit and fills
-    /// lines up to the width of the terminal with spaces if fill parameter is 
+    /// lines up to the width of the terminal with spaces if fill parameter is
     /// true.
     ///
     /// # Example
@@ -67,7 +65,12 @@ impl TermSnip{
     /// ```
     ///
     pub fn new_with_fill(limit: usize, fill: bool) -> TermSnip {
-        TermSnip{ term: Term::stdout(), limit, fill, lines: VecDeque::new()}
+        TermSnip {
+            term: Term::stdout(),
+            limit,
+            fill,
+            lines: VecDeque::new(),
+        }
     }
 
     /// Creates a TermSnip wich limits output lines to the given limit.
@@ -79,20 +82,23 @@ impl TermSnip{
     /// ```
     ///
     pub fn new(limit: usize) -> TermSnip {
-        TermSnip{ term: Term::stdout(), limit, fill: false, lines: VecDeque::new()}
+        TermSnip {
+            term: Term::stdout(),
+            limit,
+            fill: false,
+            lines: VecDeque::new(),
+        }
     }
 
     /// Writes a line to the terminal (stdout).
     pub fn write_line(&mut self, text: &str) -> io::Result<()> {
-
         // split text into multiple text when it is longer than a line
-        let line_len:usize = self.term.size().1.into();
-        
+        let line_len: usize = self.term.size().1.into();
         // if the char count of the text is less than the line length
         // just write it and return
         if text.chars().count() < line_len {
             self.write_short_line(text, line_len)?;
-            return Ok(())
+            return Ok(());
         }
         // when this code line is reached the text is larger then the line length
         // and must be splitted to be written as multiple lines
@@ -104,8 +110,8 @@ impl TermSnip{
             self.term_write_line(first)?;
         }
 
-        self.write_short_line(text, line_len)?;
-        
+        self.write_short_line(last_text, line_len)?;
+
         Ok(())
     }
 
@@ -114,7 +120,7 @@ impl TermSnip{
     /// line length it is filled with spaces.
     /// Done to workaround some strange issues when writing output of std::process
     /// in some cases.
-    fn write_short_line(&mut self, text: &str, line_len:usize) -> io::Result<()> {
+    fn write_short_line(&mut self, text: &str, line_len: usize) -> io::Result<()> {
         if self.fill && text.chars().count() <= line_len {
             self.term_write_line(&format!("{:width$}", text, width = line_len))?;
         } else {
@@ -122,7 +128,6 @@ impl TermSnip{
         }
         Ok(())
     }
-
 
     /// Delegates the writing to console::Term and manages the line limit.
     fn term_write_line(&mut self, text: &str) -> io::Result<()> {
@@ -135,16 +140,13 @@ impl TermSnip{
             }
         } else {
             self.term.write_line(text)?;
-        }        
+        }
         Ok(())
-
     }
-
 
     /// Clear the lines written with the TermSnip instance
     pub fn clear_lines(&mut self) -> io::Result<()> {
         self.term.clear_last_lines(self.lines.len())?;
         Ok(())
     }
-
 }
